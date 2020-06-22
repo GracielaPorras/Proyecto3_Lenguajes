7b98b3ee11bcf6cccbda6304b080b1d982fb8bdb
GraciPorras <porrasgraci@gmail.com>
2020-06-21 19:03:05 -0600

    Inicia sesion Centro Entrenamiento

--- Controllers/GimnasioController.cs
+++ Controllers/GimnasioController.cs
@@ -9,6 +9,8 @@ using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
 using Proyecto3LenguajesISemestre_ModuloAdmin_Graciela_Randall.Models;
 using Microsoft.AspNetCore.WebUtilities;
+using System.Web.Helpers;
+using Newtonsoft.Json.Linq;
 
 namespace Proyecto3LenguajesISemestre_ModuloAdmin_Graciela_Randall.Controllers
 {
@@ -54,6 +56,11 @@ namespace Proyecto3LenguajesISemestre_ModuloAdmin_Graciela_Randall.Controllers
             return View();
         }
 
+        public ActionResult Principal()
+        {
+            return View();
+        }
+
         // GET: GimnasioController/Details/5
         public ActionResult Details(int id)
         {
@@ -67,9 +74,9 @@ namespace Proyecto3LenguajesISemestre_ModuloAdmin_Graciela_Randall.Controllers
         }
 
 
-        public string RegistraGYM(Gimnasio GYM)
+        public ActionResult RegistraGYM(Gimnasio GYM)
         {
-            string salida = "";
+            string responseBody = "";
 
             conexion("/registrarse", "POST");
             string json = JsonSerializer.Serialize(GYM);
@@ -89,7 +96,7 @@ namespace Proyecto3LenguajesISemestre_ModuloAdmin_Graciela_Randall.Controllers
                         if (strReader == null) System.Diagnostics.Debug.WriteLine("NULL"); ;
                         using (StreamReader objReader = new StreamReader(strReader))
                         {
-                            string responseBody = objReader.ReadToEnd();
+                            responseBody = objReader.ReadToEnd();
                             // Do something with responseBody
                             System.Diagnostics.Debug.WriteLine("repuesta*************"+responseBody);
                         }
@@ -102,7 +109,71 @@ namespace Proyecto3LenguajesISemestre_ModuloAdmin_Graciela_Randall.Controllers
                 // Handle error
             }
 
-            return salida;
+            Respuesta respuesta = Newtonsoft.Json.JsonConvert.DeserializeObject<Respuesta>(responseBody);
+
+            if (respuesta.Resultado == "2")
+            {
+                return View("Principal");
+            }
+            else
+            {
+                return View("Registrarse", "El usuario ya existe");
+            }
+
+            
         }
+
+        public ActionResult IniciarGYM(Gimnasio GYM)
+        {
+            string responseBody = "";
+
+            conexion("/iniciar", "POST");
+            string json = JsonSerializer.Serialize(GYM);
+
+            StreamWriter stream = new StreamWriter(req.GetRequestStream());
+
+            stream.Write(json);
+            stream.Flush();
+            stream.Close();
+
+            try
+            {
+                using (WebResponse response = req.GetResponse())
+                {
+                    using (Stream strReader = response.GetResponseStream())
+                    {
+                        if (strReader == null) System.Diagnostics.Debug.WriteLine("NULL"); ;
+                        using (StreamReader objReader = new StreamReader(strReader))
+                        {
+                            responseBody = objReader.ReadToEnd();
+                            // Do something with responseBody
+                            System.Diagnostics.Debug.WriteLine("repuesta*************" + responseBody);
+                        }
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                System.Diagnostics.Debug.WriteLine("**********error*************");
+                // Handle error
+            };
+
+            //var obj = JObject.Parse(responseBody);
+            
+            Respuesta respuesta= Newtonsoft.Json.JsonConvert.DeserializeObject<Respuesta>(responseBody);
+            if (respuesta.Resultado == "1")
+            {
+                return View("Principal");
+            }
+            else
+            {
+                return View("IniciarSesion", "Usuario y contraseña inválidos");
+            }
+
+
+        }
+
     }
+
 }
+
